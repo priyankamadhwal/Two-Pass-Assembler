cddb122284ae53fd6c9c95e9b102fc0d30c95d54
Priyanka Madhwal <priyanka0701m@gmail.com>
2018-12-18 10:50:09 +0530

    Update AddressingModesHelper.java

--- src/java/AddressingModesHelper.java
+++ src/java/AddressingModesHelper.java
@@ -1,14 +1,14 @@
 import java.util.*;
 
-// ---------------------------------------------------------------------------------------------------------------------------------------------------------
-// ******************************************************************* ADDRESSING MODES HELPER**************************************************************
-// ---------------------------------------------------------------------------------------------------------------------------------------------------------
+// -------------------------------------------------------------------------------------------------------------------------------------
+// ****************************************************** ADDRESSING MODES HELPER ******************************************************
+// -------------------------------------------------------------------------------------------------------------------------------------
 
 class AMHelper
 {
-	// ----------------------------------------------------------------------------------------------------------------------------------------------------
-	// ************************************************************** TYPES OF ADDRESSING MODES ***********************************************************
-	// ----------------------------------------------------------------------------------------------------------------------------------------------------
+	// -----------------------------------------------------------------------------------------------------------------------------
+	// ************************************************* TYPES OF ADDRESSING MODES *************************************************
+	// -----------------------------------------------------------------------------------------------------------------------------
 	
 	static enum AM
 	{
@@ -22,24 +22,24 @@ class AMHelper
 		UNDEF				// Undefined
 	}
 	
-	// ----------------------------------------------------------------------------------------------------------------------------------------------------
+	// -----------------------------------------------------------------------------------------------------------------------------
 	
-	// ----------------------------------------------------------------------------------------------------------------------------------------------------
-	// **************************************************************** EXTERNAL DEPENDENCIES *************************************************************
-	// ----------------------------------------------------------------------------------------------------------------------------------------------------
+	// -----------------------------------------------------------------------------------------------------------------------------
+	// *************************************************** EXTERNAL DEPENDENCIES ***************************************************
+	// -----------------------------------------------------------------------------------------------------------------------------
 		
 			// MACHINE INSTRUCTION TABLE - MIT class : TO load machine instruction table.
 	
-	// ----------------------------------------------------------------------------------------------------------------------------------------------------
+	// -----------------------------------------------------------------------------------------------------------------------------
 	
-	// ----------------------------------------------------------------------------------------------------------------------------------------------------
-	// ********************************************************************** METHODS *********************************************************************
-	// ----------------------------------------------------------------------------------------------------------------------------------------------------
+	// -----------------------------------------------------------------------------------------------------------------------------
+	// ********************************************************** METHODS **********************************************************
+	// -----------------------------------------------------------------------------------------------------------------------------
 	
 	/* getAddressingMode        : 
-									OBJECTIVE 	- Return the addressing mode of given operand.
-									INPUT  		- op : String, the operand whose addressing mode we have to find.
-									OUTPUT 		- AM, addressing mode of operand. 
+						OBJECTIVE 	- Return the addressing mode of given operand.
+						INPUT  		- op : String, the operand whose addressing mode we have to find.
+						OUTPUT 		- AM, addressing mode of operand. 
 	*/
 	static AM getAddressingMode(String op)
 	{
@@ -62,50 +62,56 @@ class AMHelper
 				if (plusCount==0)
 				{
 					
-					// If no. of '+' = 0, and op is a register, then register indeirect addressing mode (eg, [CX]).
+					// If no. of '+' = 0, and op is a register, then register indeirect addressing mode.
+					// (eg, [CX])
 					if (MIT.REG.contains(op))
-						return AM.REGIND;									//REGISTER INDIRECT
+						return AM.REGIND;				//REGISTER INDIRECT
 				}
 				
 				else if (plusCount==1)
 				{
-					// If no. of '+' = 1, and op contains SI or DI, then register relative addressing mode (eg, [BX+SI]).
+					// If no. of '+' = 1, and op contains SI or DI, then register relative addressing mode.
+					// (eg, [BX+SI])
 					String o[] = op.split("\\+");
 					if (o[1].equals("SI") || o[1].equals("DI"))
-						return AM.REGREL;									//REGISTER RELATIVE
+						return AM.REGREL;				//REGISTER RELATIVE
 					
-					// If no. of '+' = 1, and op does not contain SI or DI, then based indexed addressing mode (eg, [BX+7]).
-					return AM.BASIND;										//BASED INDEXD
+					// If no. of '+' = 1, and op does not contain SI or DI, then based indexed addressing mode.
+					// (eg, [BX+7])
+					return AM.BASIND;					//BASED INDEXD
 				}
 				else if (plusCount==2)
 				{
-					// If no. of '+' = 2, then relative based indexed addressing mode (eg, [BX+SI+7]).
-					return AM.RELBASIND;									//RELATIVE BASED INDEXED
+					// If no. of '+' = 2, then relative based indexed addressing mode.
+					// (eg, [BX+SI+7])
+					return AM.RELBASIND;					//RELATIVE BASED INDEXED
 				}
 				else
 				{
 					// Error.
 					Pass1.errorFlag = true;
-					return AM.UNDEF;										//UNDEFINED
+					return AM.UNDEF;					//UNDEFINED
 				}
 			}
 			else
 			{
 				// Error.
 				Pass1.errorFlag = true;
-				return AM.UNDEF;											//UNDEFINED
+				return AM.UNDEF;						//UNDEFINED
 			}
 		}
 		
 		if (MIT.REG.contains(op) || MIT.SREG.contains(op))
-			// If op is a register, then register addressing mode (eg, DX).
-			return AM.REG;													//REGISTER
+			// If op is a register, then register addressing mode.
+			// (eg, DX)
+			return AM.REG;								//REGISTER
 		
 		if (op.charAt(0)=='#')
-			// If op starts with '#', then immediate addressing mode (eg, #6DH).
+			// If op starts with '#', then immediate addressing mode.
+			// (eg, #6DH)
 		{
 			
-			// HEXADECIMAL VALUE											// IMMEDIATE
+			// HEXADECIMAL VALUE							// IMMEDIATE
 			if (op.charAt(op.length()-1)=='H')
 			{
 				op=op.substring(1,op.length()-1);
@@ -135,11 +141,12 @@ class AMHelper
 				System.out.print("Decimal ");
 				return AM.IMM;
 			}
-			return AM.UNDEF;												//UNDEFINED
+			return AM.UNDEF;							//UNDEFINED
 		}
 		else
-			// Else direct addressing mode (eg, DATA).
-			return AM.DIR;													//DIRECT
+			// Else direct addressing mode.
+			// (eg, DATA)
+			return AM.DIR;								//DIRECT
 	}
 	
 	static boolean isNumeric(String str){
@@ -153,18 +160,19 @@ class AMHelper
 		}
 	}
 
-	static final AM[] mem_am = {AM.DIR, AM.REGIND, AM.REGREL, AM.BASIND, AM.RELBASIND};		//Memory references
-	static final AM[] reg_am = {AM.REG};													//Register
-	static final AM[] imm_am = {AM.IMM};													//Immediate
+	static final AM[] mem_am = {AM.DIR, AM.REGIND, AM.REGREL, AM.BASIND, AM.RELBASIND};			//Memory references
+	static final AM[] reg_am = {AM.REG};									//Register
+	static final AM[] imm_am = {AM.IMM};									//Immediate
 	
 	/* addressingModeIntermediate: 
-									OBJECTIVE 	- An intermediate function to return the operand type based on addressing mode.
-									INPUT	 	- OP : String, operand whose type is to be found.
-									OUTPUT 		- String, operand type:
-													REG		- 	General purpose register
-													SREG	-	Segment register
-													MEM		- 	Memory
-													IMM		- 	Immediate
+						OBJECTIVE 	- An intermediate function to return the operand type based on 
+						            	  addressing mode.
+						INPUT	 	- OP : String, operand whose type is to be found.
+						OUTPUT 		- String, operand type:
+								  REG	- 	General purpose register
+								  SREG	-	Segment register
+								  MEM	- 	Memory
+								  IMM	- 	Immediate
 	*/
 	static String addressingModeIntermediate(String OP)
 	{
@@ -175,14 +183,14 @@ class AMHelper
 		// Print addressing modes.
 		switch (amOP)
 		{
-			case IMM		: 	System.out.println(OP+" immediate "); 				break;
-			case DIR		:	System.out.println(OP+" direct "); 					break;		
-			case REG		:	System.out.println(OP+" register ");			 	break;			
+			case IMM		: 	System.out.println(OP+" immediate "); 			break;
+			case DIR		:	System.out.println(OP+" direct "); 			break;		
+			case REG		:	System.out.println(OP+" register ");			break;			
 			case REGIND		:	System.out.println(OP+" register indirect "); 		break;		
 			case REGREL		:	System.out.println(OP+" register relative "); 		break;		
-			case BASIND		:	System.out.println(OP+" based indexed "); 			break;	
-			case RELBASIND	:	System.out.println(OP+" relative based indexed ");	break;
-			case UNDEF		:	System.out.println(OP+" undefined "); 				break;
+			case BASIND		:	System.out.println(OP+" based indexed "); 		break;	
+			case RELBASIND		:	System.out.println(OP+" relative based indexed ");	break;
+			case UNDEF		:	System.out.println(OP+" undefined "); 			break;
 		}
 		
 		// To store operand type
@@ -215,7 +223,7 @@ class AMHelper
 		return type;
 	}
 	
-	// ----------------------------------------------------------------------------------------------------------------------------------------------------
-	// ************************************************************************ END ***********************************************************************
-	// ----------------------------------------------------------------------------------------------------------------------------------------------------
+	// -----------------------------------------------------------------------------------------------------------------------------
+	// ************************************************************ END ************************************************************
+	// -----------------------------------------------------------------------------------------------------------------------------
 }
